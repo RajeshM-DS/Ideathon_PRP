e865748219b469b88c032b95ae7869d0c94da2f6
Rajes RM <rmrajeshmanickam1@gmail.com>
2020-06-29 10:48:38 +0530

    new swg changes

--- swag.py
+++ swag.py
@@ -6,7 +6,7 @@ Created on Sun Jun 21 23:04:59 2020
 """
 
 import sys
-sys.path.append("D:\\Rajesh\\Ideathon\\Patient Retention Predictor\\Program")
+sys.path.append("D:\\Ideathon_PRP-master")
 
 import json
 import numpy as np
@@ -14,21 +14,23 @@ from flask import Flask, request, jsonify
 from flasgger import Swagger
 from flasgger.utils import swag_from
 from flasgger import LazyString, LazyJSONEncoder
+from flask_cors import CORS
 
 
 import pickle
 import pandas as pd
 
-model = pickle.load(open('D:\\Rajesh\\Ideathon\\Patient Retention Predictor\\Program\\engine.pkl', 'rb'))
+model = pickle.load(open('D:\\Ideathon_PRP-master\\engine.pkl', 'rb'))
 
-model_columns = pickle.load(open('D:\\Rajesh\\Ideathon\\Patient Retention Predictor\\Program\\engine_columns.pkl', 'rb'))
+model_columns = pickle.load(open('D:\\Ideathon_PRP-master\\engine_columns.pkl', 'rb'))
 
-model2 = pickle.load(open('D:\\Rajesh\\Ideathon\\Patient Retention Predictor\\Program\\engine_enrollment.pkl', 'rb'))
+model2 = pickle.load(open('D:\\Ideathon_PRP-master\\engine_enrollment.pkl', 'rb'))
 
-model2_columns = pickle.load(open('D:\\Rajesh\\Ideathon\\Patient Retention Predictor\\Program\\engine_enrollment_columns.pkl', 'rb'))
+model2_columns = pickle.load(open('D:\\Ideathon_PRP-master\\engine_enrollment_columns.pkl', 'rb'))
 
 
 app = Flask(__name__)
+CORS(app)
 app.config["SWAGGER"] = {"title": "Swagger-UI", "uiversion": 2}
 
 swagger_config = {
@@ -61,78 +63,78 @@ swagger = Swagger(app, config=swagger_config, template=template)
 
 
 @app.route("/patient_prediction", methods=["POST"])
-@swag_from("D:\\Rajesh\\Ideathon\\Patient Retention Predictor\\Program\\swagger_config.yml")
+@swag_from("D:\\Ideathon_PRP-master\\swagger_config.yml")
 def patient_prediction():
     input_json = request.get_json()
-    try:
-        v1=int(input_json["Visits delay"])
-        v2=int(input_json['Age'])
-        v3=str(input_json['Race'])
-        v4=str(input_json['Sex'])
-        v5=str(input_json['AE Intensity'])
-        v6=str(input_json['Action taken'])
-        v7=int(input_json['Recovery duration'])
-        v8=str(input_json['Tumor Lesion'])
-        v9=str(input_json['Tumor Location'])
-        v10=str(input_json['Fatal'])
-        v11=str(input_json['Pregnancy Test'])
-        v12=str(input_json['Dipstick test'])
-        v13=str(input_json['Hepatitis B-signal per cutoff (s/c)'])
-        v14=str(input_json['atopic H'])
-        v15=str(input_json['Surgical H'])
-        v16=str(input_json['Vaccination History'])
-        v17=str(input_json['Concometant Drug/Medications'])
-        v18=float(input_json['PK Clearance'])
-        v19=str(input_json['Asthma Exac.Defin'])
-        v20=str(input_json['Spirometry'])
-        v21=int(input_json['Smoking History (/week)'])
-        v22=int(input_json['Alcohol habits (/week-ml)'])
-        v23=float(input_json['Creatinine_mg/dL'])
-        v24=int(input_json['Sodium_mmol/L'])
-        v25=float(input_json['Potassium_mmol/L'])
-        v26=float(input_json['Bilirubin_mg/dL'])
-        v27=int(input_json['AST_U/L'])
-        v28=int(input_json['ALT_U/L'])
-        v29=int(input_json['Creatine Kinase_U/L'])
-        v30=int(input_json['Alkaline Phosphatase_U/L'])
-        v31=int(input_json['C-Reactive Protein_mg/L'])
-        v32=float(input_json['Hemoglobin_g/dL'])
-        v33=float(input_json['Leukocytes_10_3/uL'])
-        v34=int(input_json['Neutrophils_cells/uL'])
-        v35=int(input_json['Eosinophils_cells/uL'])
-        v36=int(input_json['Basophils_cells/uL'])
-        v37=int(input_json['Lymphocytes_cells/uL'])
-        v38=int(input_json['Monocytes_cells/uL'])
-        v39=int(input_json['Platelets_10_3/uL'])
-        v40=int(input_json['MCV_fL'])
-        v41=float(input_json['Erythrocytes_M/uL'])
-        v42=int(input_json['Hematocrit_%'])
-        
-        
-        data=[[v1,v2,v3,v4,v5,v6,v7,v8,v9,v10,v11,v12,v13,v14,v15,v16,v17,v18,v19,v20,v21,v22,v23,v24,v25,v26,v27,v28,v29,v30,v31,v32,v33,v34,v35,v36,v37,v38,v39,v40,v41,v42]]
+    
+    v1=int(input_json["Visits_delay"])
+    v2=int(input_json['Age'])
+    v3=str(input_json['Race'])
+    v4=str(input_json['Sex'])
+    v5=str(input_json['AE_Intensity'])
+    v6=str(input_json['Action_taken'])
+    v7=int(input_json['Recovery_duration'])
+    v8=str(input_json['Tumor_Lesion'])
+    v9=str(input_json['Tumor_Location'])
+    v10=str(input_json['Fatal'])
+    v11=str(input_json['Pregnancy_Test'])
+    v12=str(input_json['Dipstick_test'])
+    v13=str(input_json['Hepatitis_Bsignal_per_cutoff'])
+    v14=str(input_json['atopic_H'])
+    v15=str(input_json['Surgical_H'])
+    v16=str(input_json['Vaccination_History'])
+    v17=str(input_json['Concometant_Drug_Medications'])
+    v18=float(input_json['PK_Clearance'])
+    v19=str(input_json['Asthma_Exac_Defin'])
+    v20=str(input_json['Spirometry'])
+    v21=int(input_json['Smoking_History'])
+    v22=int(input_json['Alcohol_habits'])
+    v23=float(input_json['Creatinine_mg_dL'])
+    v24=int(input_json['Sodium_mmol_L'])
+    v25=float(input_json['Potassium_mmol_L'])
+    v26=float(input_json['Bilirubin_mg_dL'])
+    v27=int(input_json['AST_U_L'])
+    v28=int(input_json['ALT_U_L'])
+    v29=int(input_json['Creatine_Kinase_U_L'])
+    v30=int(input_json['Alkaline_Phosphatase_U_L'])
+    v31=int(input_json['C_Reactive_Protein_mg_L'])
+    v32=float(input_json['Hemoglobin_g_dL'])
+    v33=float(input_json['Leukocytes_10_3_uL'])
+    v34=int(input_json['Neutrophils_cells_uL'])
+    v35=int(input_json['Eosinophils_cells_uL'])
+    v36=int(input_json['Basophils_cells_uL'])
+    v37=int(input_json['Lymphocytes_cells_uL'])
+    v38=int(input_json['Monocytes_cells_uL'])
+    v39=int(input_json['Platelets_10_3_uL'])
+    v40=int(input_json['MCV_fL'])
+    v41=float(input_json['Erythrocytes_M_uL'])
+    v42=int(input_json['Hematocrit'])
+    
+    
+    data=[[v1,v2,v3,v4,v5,v6,v7,v8,v9,v10,v11,v12,v13,v14,v15,v16,v17,v18,v19,v20,v21,v22,v23,v24,v25,v26,v27,v28,v29,v30,v31,v32,v33,v34,v35,v36,v37,v38,v39,v40,v41,v42]]
 
-        col=['Visits delay','Age','Race','Sex','AE Intensity','Action taken','Recovery duration','Tumor Lesion','Tumor Location','Fatal','Pregnancy Test','Dipstick test','Hepatitis B-signal per cutoff (s/c)','atopic H','Surgical H','Vaccination History','Concometant Drug/Medications','PK Clearance','Asthma Exac.Defin','Spirometry','Smoking History (/week)','Alcohol habits (/week-ml)','Creatinine_mg/dL','Sodium_mmol/L','Potassium_mmol/L','Bilirubin_mg/dL','AST_U/L','ALT_U/L','Creatine Kinase_U/L','Alkaline Phosphatase_U/L','C-Reactive Protein_mg/L','Hemoglobin_g/dL','Leukocytes_10_3/uL','Neutrophils_cells/uL','Eosinophils_cells/uL','Basophils_cells/uL','Lymphocytes_cells/uL','Monocytes_cells/uL','Platelets_10_3/uL','MCV_fL','Erythrocytes_M/uL','Hematocrit_%']
-        df = pd.DataFrame(data, columns = col) 
+    col=['Visits delay','Age','Race','Sex','AE Intensity','Action taken','Recovery duration','Tumor Lesion','Tumor Location','Fatal','Pregnancy Test','Dipstick test','Hepatitis B-signal per cutoff (s/c)','atopic H','Surgical H','Vaccination History','Concometant Drug/Medications','PK Clearance','Asthma Exac.Defin','Spirometry','Smoking History (/week)','Alcohol habits (/week-ml)','Creatinine_mg/dL','Sodium_mmol/L','Potassium_mmol/L','Bilirubin_mg/dL','AST_U/L','ALT_U/L','Creatine Kinase_U/L','Alkaline Phosphatase_U/L','C-Reactive Protein_mg/L','Hemoglobin_g/dL','Leukocytes_10_3/uL','Neutrophils_cells/uL','Eosinophils_cells/uL','Basophils_cells/uL','Lymphocytes_cells/uL','Monocytes_cells/uL','Platelets_10_3/uL','MCV_fL','Erythrocytes_M/uL','Hematocrit_%']
+    df = pd.DataFrame(data, columns = col) 
 
-        df_user = pd.get_dummies(df)
-        
-        for col in model_columns:
-            if col not in df_user.columns:
-                df_user[col] = 0
+    df_user = pd.get_dummies(df)
+    
+    for col in model_columns:
+        if col not in df_user.columns:
+            df_user[col] = 0
 
-        if model.predict_classes(df_user) == 1:
-            result='Patient will drop out from the oncology trial'
-        else:
-            result='Patient will not drop out from the oncology trial'
+    if model.predict_classes(df_user) == 1:
+        result='Patient will drop out from the oncology trial'
+    else:
+        result='Patient will not drop out from the oncology trial'
 
-    except:
-        result='error'
+    #except:
+     #   result='error'
     return json.dumps(result)
 
 
 
 @app.route("/patient_enrollment", methods=["POST"])
-@swag_from("D:\\Rajesh\\Ideathon\\Patient Retention Predictor\\Program\\swagger_config_enrollment.yml")
+@swag_from("D:\\Ideathon_PRP-master\\swagger_config_enrollment.yml")
 def patient_enrollment():
     input_json = request.get_json()
     try:
@@ -141,40 +143,40 @@ def patient_enrollment():
         v2=str(input_json['Race'])
         v3=str(input_json['Sex'])
         v4=str(input_json['BMI'])
-        v5=str(input_json['Blood pressure'])
-        v6=str(input_json['Heart rate'])
-        v7=str(input_json['Pregnancy Test'])
-        v8=str(input_json['Dipstick test'])
-        v9=str(input_json['Hepatitis B-signal per cutoff (s/c)'])
-        v10=str(input_json['atopic H'])
-        v11=str(input_json['Surgical H'])
-        v12=str(input_json['Vaccination History'])
-        v13=str(input_json['Concometant Drug/Medications'])
-        v14=float(input_json['PK Clearance'])
-        v15=str(input_json['Asthma Exac.Defin'])
+        v5=str(input_json['Blood_pressure'])
+        v6=str(input_json['Heart_rate'])
+        v7=str(input_json['Pregnancy_Test'])
+        v8=str(input_json['Dipstick_test'])
+        v9=str(input_json['Hepatitis_Bsignal_per_cutoff'])
+        v10=str(input_json['atopic_H'])
+        v11=str(input_json['Surgical_H'])
+        v12=str(input_json['Vaccination_History'])
+        v13=str(input_json['Concometant_Drug_Medications'])
+        v14=float(input_json['PK_Clearance'])
+        v15=str(input_json['Asthma_Exac_Defin'])
         v16=str(input_json['Spirometry'])
-        v17=int(input_json['Smoking History (/week)'])
-        v18=int(input_json['Alcohol habits (/week-ml)'])
-        v19=float(input_json['Creatinine_mg/dL'])
-        v20=int(input_json['Sodium_mmol/L'])
-        v21=float(input_json['Potassium_mmol/L'])
-        v22=float(input_json['Bilirubin_mg/dL'])
-        v23=int(input_json['AST_U/L'])
-        v24=int(input_json['ALT_U/L'])
-        v25=int(input_json['Creatine Kinase_U/L'])
-        v26=int(input_json['Alkaline Phosphatase_U/L'])
-        v27=int(input_json['C-Reactive Protein_mg/L'])
-        v28=float(input_json['Hemoglobin_g/dL'])
-        v29=float(input_json['Leukocytes_10_3/uL'])
-        v30=int(input_json['Neutrophils_cells/uL'])
-        v31=int(input_json['Eosinophils_cells/uL'])
-        v32=int(input_json['Basophils_cells/uL'])
-        v33=int(input_json['Lymphocytes_cells/uL'])
-        v34=int(input_json['Monocytes_cells/uL'])
-        v35=int(input_json['Platelets_10_3/uL'])
+        v17=int(input_json['Smoking_History'])
+        v18=int(input_json['Alcohol_habits'])
+        v19=float(input_json['Creatinine_mg_dL'])
+        v20=int(input_json['Sodium_mmol_L'])
+        v21=float(input_json['Potassium_mmol_L'])
+        v22=float(input_json['Bilirubin_mg_dL'])
+        v23=int(input_json['AST_U_L'])
+        v24=int(input_json['ALT_U_L'])
+        v25=int(input_json['Creatine_Kinase_U_L'])
+        v26=int(input_json['Alkaline_Phosphatase_U_L'])
+        v27=int(input_json['C_Reactive_Protein_mg_L'])
+        v28=float(input_json['Hemoglobin_g_dL'])
+        v29=float(input_json['Leukocytes_10_3_uL'])
+        v30=int(input_json['Neutrophils_cells_uL'])
+        v31=int(input_json['Eosinophils_cells_uL'])
+        v32=int(input_json['Basophils_cells_uL'])
+        v33=int(input_json['Lymphocytes_cells_uL'])
+        v34=int(input_json['Monocytes_cells_uL'])
+        v35=int(input_json['Platelets_10_3_uL'])
         v36=int(input_json['MCV_fL'])
-        v37=float(input_json['Erythrocytes_M/uL'])
-        v38=int(input_json['Hematocrit_%'])
+        v37=float(input_json['Erythrocytes_M_uL'])
+        v38=int(input_json['Hematocrit'])
         
         data_2=[[v1,v2,v3,v4,v5,v6,v7,v8,v9,v10,v11,v12,v13,v14,v15,v16,v17,v18,v19,v20,v21,v22,v23,v24,v25,v26,v27,v28,v29,v30,v31,v32,v33,v34,v35,v36,v37,v38]]
     
